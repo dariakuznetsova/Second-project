0586baecb506ceaa4a5070427701e6cebe031d3a
Daria Kuznetsova <kds2108list.ru>
2016-02-22 21:12:14 +0300

    еще один коммит

--- site/js/main.js
+++ site/js/main.js
@@ -1,18 +1,29 @@
+// Аккордеон
 var acco = (function () {
 
     var init = function () {
-        arrow(),
         _setUpListners();
     };
 
-    var arrow = function() {
-        if ($('acco__content').css('display') !== 'none') {
-            $('.acco__link:hover:after').css('@include sprite($arrow-up);');
-            console.log('открыто');
-        }else{
-            console.log('закрыто');
-        };
-    };
+    /*var arrow = function() {
+        var acco__content = $('.acco__content');
+
+        if (acco__content) {
+            $('.acco__link').hover (function() {
+                $(this).addClass('up');
+                }, function() {
+                 $(this).removeClass('up');
+                });
+            console.log('open')
+        }else if(!acco__content){
+            $('.acco__link').hover (function() {
+                $(this).addClass('down');
+                }, function() {
+                $(this).removeClass('down');
+                });
+            console.log('close') 
+            };
+    };*/
 
     var _setUpListners = function ()  {
     
@@ -22,11 +33,16 @@ var acco = (function () {
 
         $('.acco__link').on ('click', function() {
             $(this).next().slideToggle();
-            console.log('hey');
+            arrow();
         });
 
-     };
+        $('.label').on ('click', function(e) {
+            if($(e).hasClass('label__radio')) {
+                $(this).addClass('label__radio_click');   
+            };
+        });
 
+};
 	return {
   		init: init, 
   	};
@@ -34,3 +50,15 @@ var acco = (function () {
 })();
 
 acco.init();
+
+//Слайдер
+
+
+
+var sl = function () {
+    $( ".slider" ).slider({
+        range: true
+    });
+    $( ".slider" ).slider( "option", "range", true );
+};
+sl();
